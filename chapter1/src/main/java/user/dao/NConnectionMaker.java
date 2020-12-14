70ddc839776753221734544d16124cfbda4c327a
황준호 <42054054+hwanghe159@users.noreply.github.com>
2020-12-14 15:49:16 +0900

    refactor: 구현체에 대한 관심을 끊기 위해 ConnectionMaker 인터페이스 적용

--- chapter1/src/main/java/user/dao/NConnectionMaker.java
+++ chapter1/src/main/java/user/dao/NConnectionMaker.java
@@ -4,8 +4,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class SimpleConnectionMaker {
-    public Connection makeNewConnection() throws ClassNotFoundException, SQLException {
+public class NConnectionMaker implements ConnectionMaker {
+    @Override
+    public Connection makeConnection() throws ClassNotFoundException, SQLException {
+        // N 사의 DB connection 생성코드
         Class.forName("org.h2.Driver");
         return DriverManager.getConnection("jdbc:h2:~/test", "sa", "");
     }
